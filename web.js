2a11dad4445735329d620610dd43314f012a9952
Simon Whittemore <simon.whittemore@gmail.com>
2013-08-16 18:46:09 +0100

    twitter bootstrap

--- web.js
+++ web.js
@@ -4,16 +4,16 @@ var path = require('path');
 var jade = require('jade');
 var fs = require('fs');
 
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
 app.set('view engine', 'jade');
 app.set('view options', {'layout': false});
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-  app.locals.pretty = true;
-});
+// app.configure('development', function(){
+//   app.use(express.errorHandler());
+//   app.locals.pretty = true;
+// });
 
 app.get('/', function(req, res) {
 	fs.readdir('public', function(error, files) {
@@ -25,10 +25,10 @@ app.get('/', function(req, res) {
 				output.push({path : files[i], title: files[i].split('.')[0]});
 			}
 		}
-		//res.send(output);
-		res.render('index', {files: output});
+		res.render('index', {files: output, title: "Tech Notes"});
 	}
 )});
+
 var port = process.env.PORT || 8088;
 app.listen(port); 
 console.log('Listening on port ' + port);
